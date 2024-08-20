f7ba7e9be6efe058129ad6e1a0a9e6b6c7cb8362
Khatanzorig <khatanzorig.b@tavanbogd.com>
2024-08-21 02:23:27 +0800

    vim updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    outDir: 'dist', // Ensure this matches your build script
-    sourcemap: false, // Disable source maps in production for faster builds
-    minify: 'terser', // Use Terser for better minification
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Optimize chunk splitting
-      }
-    }
-  }
-});
+  plugins: [reactRefresh()]
+})
